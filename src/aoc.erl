372bf73638affe77cbedc60ed90e0c8b8622062c
Björn Schulz <bjoern@fac3.org>
2023-12-09 14:34:30 +0100

    Add some more commands to aoc

--- src/aoc.erl
+++ src/aoc.erl
@@ -5,6 +5,18 @@
 
 
 -spec main([string()]) -> ok.
+main(["list"]) ->
+    maybe
+        {ok, Days} = find_days(),
+        io:format("~s~n", [lists:join(" ", Days)])
+    end;
+
+main(["all"]) ->
+    maybe
+        {ok, Days} = find_days(),
+        lists:foreach(fun(Day) -> main([Day]) end, Days)
+    end;
+
 main([Day]) ->
     Root = filename:dirname(filename:dirname(code:which(?MODULE))),
     DayDir = lists:flatten(["day-", string:pad(Day, 2, leading, "0")]),
@@ -12,7 +24,8 @@ main([Day]) ->
     main([Day, Path]);
 
 main([Day, Filename]) ->
-    ModuleName = lists:flatten(["day_", string:pad(Day, 2, leading, "0")]),
+    DayNo = string:pad(Day, 2, leading, "0"),
+    ModuleName = lists:flatten(["day_", DayNo]),
     maybe
         {ok, Data} ?= file:read_file(Filename),
         Lines = binary:split(Data, <<"\n">>, [global, trim_all]),
@@ -22,7 +35,7 @@ main([Day, Filename]) ->
                 Result = catch Module:run(Part, Lines),
                 Stop = erlang:monotonic_time(nanosecond),
                 Duration = (Stop - Start) / 1000000,
-                io:format("~p ~.3fms: ~p~n", [Part, Duration, Result])
+                io:format("~s ~p ~.3fms: ~p~n", [DayNo, Part, Duration, Result])
             end,
             Module:parts())
     else
@@ -31,4 +44,17 @@ main([Day, Filename]) ->
     end;
 
 main(_) ->
-    io:format("usage: aoc day [filename]~n").
+    io:format("usage: aoc [DAY [FILENAME]|list|all]~n").
+
+
+find_days() ->
+    maybe
+        {ok, Filenames} ?= file:list_dir(filename:dirname(code:which(?MODULE))),
+        {ok, lists:sort(lists:filtermap(fun(Filename) ->
+                case re:run(Filename, "day_(\\d+)\\.beam", [{capture, all_but_first, list}]) of
+                    {match, [Day]} -> {true, Day};
+                    _ -> false
+                end
+            end,
+            Filenames))}
+    end.

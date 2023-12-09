add0951b8c1e764aa7a0e3c6be666236fb11a24f
Björn Schulz <bjoern@fac3.org>
2023-12-09 12:39:43 +0100

    Restructure day 7

--- src/day_07.erl
+++ src/day_07.erl
@@ -1,17 +1,25 @@
-#!/usr/bin/env escript
+-module(day_07).
+
+-export([parts/0, run/2]).
 
 
--spec main([string()]) -> ok.
-main([Filename]) ->
-    {ok, Data} = file:read_file(Filename),
-    Lines = binary:split(Data, <<"\n">>, [global, trim_all]),
+-spec parts() -> [atom()].
+parts() -> [one, two].
+
+
+-spec run(one | two, [binary()]) -> integer().
+run(one, Lines) ->
     Hands = parse_hands(Lines),
-    Result = process_hands(Hands),
-    Sum = lists:sum(Result),
-    io:format("~p~n", [Sum]);
+    ClassifiedHands = classify_hands_one(Hands),
+    Result = process_hands(ClassifiedHands),
+    lists:sum(Result);
 
-main(_) ->
-    io:format("usage: escript solution.escript filename~n").
+run(two, Lines) ->
+    Hands = parse_hands(Lines),
+    DevaluedJokers = [{lists:map(fun(11) -> 1; (C) -> C end, Cards), Bid} || {Cards, Bid} <- Hands],
+    ClassifiedHands = classify_hands_two(DevaluedJokers),
+    Result = process_hands(ClassifiedHands),
+    lists:sum(Result).
 
 
 parse_hands(Lines) ->
@@ -24,19 +32,18 @@ parse_hands([], Result) ->
 parse_hands([Line | Lines], Result) ->
     [Cards, Bid] = binary:split(Line, <<" ">>, [global, trim_all]),
     ParsedCards = parse_cards(Cards),
-    parse_hands(Lines, [{ParsedCards,
-                         classify_cards(ParsedCards),
-                         binary_to_integer(Bid)} | Result]).
+    parse_hands(Lines, [{ParsedCards, binary_to_integer(Bid)} | Result]).
+
 
 parse_cards(Card) ->
-    parse_cards(binary_to_list(Card),[]).
+    parse_cards(binary_to_list(Card), []).
 
 
 parse_cards([], Result) -> lists:reverse(Result);
 parse_cards([$A | Cards], Result) -> parse_cards(Cards, [14 | Result]);
 parse_cards([$K | Cards], Result) -> parse_cards(Cards, [13 | Result]);
 parse_cards([$Q | Cards], Result) -> parse_cards(Cards, [12 | Result]);
-parse_cards([$J | Cards], Result) -> parse_cards(Cards, [1 | Result]);
+parse_cards([$J | Cards], Result) -> parse_cards(Cards, [11 | Result]);
 parse_cards([$T | Cards], Result) -> parse_cards(Cards, [10 | Result]);
 parse_cards([C | Cards], Result) -> parse_cards(Cards, [C - $0 | Result]).
 
@@ -58,7 +65,27 @@ compare_cards([C | CardsA], [C | CardsB]) -> compare_cards(CardsA, CardsB);
 compare_cards([A | _], [B | _]) -> A =< B.
 
 
-classify_cards(Cards) ->
+classify_hands_one(Hands) ->
+    [{Cards, classify_cards_one(Cards), Bid} || {Cards, Bid} <- Hands].
+
+
+classify_hands_two(Hands) ->
+    [{Cards, classify_cards_two(Cards), Bid} || {Cards, Bid} <- Hands].
+
+
+classify_cards_one(Cards) ->
+    case lists:reverse(lists:sort(maps:values(utils:frequencies(Cards)))) of
+        [5] -> 6; % five of a kind
+        [4, _] -> 5; % four of a kind
+        [3, 2] -> 4; % full house
+        [3 | _] -> 3; % three of a kind
+        [2, 2, _] -> 2; % two pair
+        [2 | _] -> 1; % one pair
+        _ -> 0 % high card
+    end.
+
+
+classify_cards_two(Cards) ->
     case lists:sort(Cards) of
         [X, X, X, X, X] -> 6; % five of a kind
         [1, X, X, X, X] -> 6; % five of a kind with joker
